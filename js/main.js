e5c0e648acd369511a2e7f41964a10a5d6ff10c3
MrOmachi <omachi.john@yahoo.com>
2022-04-18 02:22:01 +0100

    Poketo Clone

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$('#theTarget').skippr({
+  transition: 'slide',
+  speed: 1000,
+  easing: 'easeOutCirc',
+  navType: 'bubble',
+  childrenElementType: 'div',
+  arrows: false,
+  autoPlay: true,
+  autoPlayDuration: 2000,
+  keyboardOnAlways: false,
+  hidePrevious: true,
+});
